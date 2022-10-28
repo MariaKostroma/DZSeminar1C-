b8c2e9f4fa8beb8d517366e8a3fed116f4da6f62
Maria <kostroma-mn@yandex.ru>
2022-10-28 15:50:24 +0300

    Определение является ли число чётным

---
+++ Seminar1DZ_Zadacha6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0){
+    Console.WriteLine("Это чётное число");
+}
+else {
+Console.WriteLine("Это нечётное число");
+}
