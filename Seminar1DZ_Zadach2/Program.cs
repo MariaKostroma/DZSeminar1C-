796d4a4289535bb671780ef79d1c9ffe94738238
Maria <kostroma-mn@yandex.ru>
2022-10-29 17:24:42 +0300

    Нахождение max и min из двух чисел

---
+++ Seminar1DZ_Zadach2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача2
+Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 < num2){
+    Console.Write("min=");
+    Console.WriteLine(num1);
+    Console.Write("max=");
+    Console.Write(num2);
+}
+else {
+    Console.Write("min=");
+    Console.WriteLine(num2);
+    Console.Write("max=");
+    Console.WriteLine(num1);
+}
