f98145161c5c33e90c272fd653af9c78c615fee0
Maria <kostroma-mn@yandex.ru>
2022-10-29 16:49:48 +0300

    Выводит чётные числа от 1 до N

---
+++ Seminar1DZ_Zadacha8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N<=0){
+   Console.WriteLine("Число меньше 1"); 
+}
+int num = 2;
+while (num<=N)
+{
+ if (num%2==0){
+ Console.Write(num + ", ");   
+}
+num++;
+}
+if (N==2){
+    Console.WriteLine(N);
+}
